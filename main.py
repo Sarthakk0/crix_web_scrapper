36c0e431193035a310666a6a7c674d6eff6976db
Sarthak Gupta <sarthak00451@gmail.com>
2024-11-14 14:03:44 +0530

    minor changes 2

--- main.py
+++ main.py
@@ -73,9 +73,9 @@ def get_data():
                     match_info = get_match_schedule(match_link+"/info")
                     print(match_info)
                 except:
+                    match_info = "NA"
                     print("Unable to get match schedule try again later")
                 livescore = "NA"
-                match_info = "NA"
                 p11 = "NA"
                 scorecard = "NA"
             result.append(
